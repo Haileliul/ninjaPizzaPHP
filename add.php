1b980f374c28fc52a66cca46554448c1a5580cde
Haileliul <hailetechlover@gmail.com>
2023-04-01 14:02:12 +0300

    validation has been complited

--- add.php
+++ add.php
@@ -1,16 +1,20 @@
-<?php
- if(isset($_POST['submit'])){
-  
-  
+ <?php
+ $title=$email=$ingridiant= '';
+ $errors = [
+  'email' => '',
+  'title' => '' ,
+  'ingredient' => '',
+ ];
 
+ if(isset($_POST['submit'])){
 
   if(empty($_POST['email'])){
-    echo "An Email is required <br/>";
+    $errors['email']= "An Email is required ";
   }else{
     $email = $_POST['email'];
     if(!filter_var($email,FILTER_VALIDATE_EMAIL))
     {
-      echo "The Valid Email must be entered";
+       $errors['email'] ="The Valid Email must be entered";
     }
     else {
       echo htmlspecialchars($_POST['email']). "<br>";
@@ -18,12 +22,12 @@
    
   }
   if(empty($_POST['title'])){
-    echo "The title is required <br>";
+    $errors['title']= "The title is required ";
   }else{
     $title = $_POST['title'];
     if(!preg_match('/^[a-zA-Z\s]+$/', $title))
     {
-      echo "Enter valid title";
+      $errors['title']="Enter valid title";
     }else {
       echo htmlspecialchars($_POST['title'])."<br>";
     }
@@ -31,12 +35,12 @@
     
   }
   if(empty($_POST['ingridiant'])){
-    echo "This ingridiant is required <br>";
+    $errors['ingredient']="This ingridiant is required ";
   }else{
     $ingridiant = $_POST['ingridiant'];
     if(!preg_match('/^\s*\w+(?:\s*,\s*\w+)*\s*$/',$ingridiant))
     {
-      echo "Enter the valid ingerediant";
+       $errors['ingredient']="Enter the valid ingerediant";
     }
     else {
       echo htmlspecialchars($_POST['ingridiant'])."<br>";
@@ -56,11 +60,17 @@
   <div class="container bg-white mx-auto px-50 max-w-lg">
     <form action="add.php" method= "POST" class ="py-10 px-10" >
           <label for=""> Your Email</label><br>
-          <input type="text" name= "email" class="border-b-2 border-r-4  focus:outline-none w-full " ><br><br>
+          <input type="text" name= "email" value="<?php echo $email?>" class="border-b-2 border-r-4  focus:outline-none w-full " >
+          <div class="text-red-200"> <?php echo $errors['email'] ?> </div>
+          <br><br>
           <label for="">Pizza Title</label><br>
-          <input type="text" name="title" class="border-b-2 border-r-4 focus:outline-none  w-full"><br><br>
+          <input type="text" name="title" value="<?php echo $title?>" class="border-b-2 border-r-4 focus:outline-none  w-full">
+           <div class="text-red-200"><?php echo $errors['title']?></div>
+          <br><br>
           <label for="">Ingrediants (Comma separated)</label><br>
-          <input type="text" name="ingridiant" class="border-b-2 border-r-4 focus:outline-none  w-full" ><br><br>
+          <input type="text" name="ingridiant" value="<?php echo $ingridiant ?>" class="border-b-2 border-r-4 focus:outline-none  w-full" >
+           <div class="text-red-200 "> <?php echo $errors['ingredient'] ?></div> 
+          <br><br>
           <button type= "submit" name="submit" class="bg-blue-300 hover:bg-orange-200 px-10 py-2 border-b-2 border-r-4 border-pink-300 text-lg  "> Submit </button>
     </form>
   </div>
